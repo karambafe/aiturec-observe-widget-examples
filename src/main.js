61c471ebbfbaa223d418641d23eddb8323acb36c
Vitaly Blokhin <karambafe@gmail.com>
2020-08-04 17:11:15 +0300

    add comments, remove lodash and core-js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import 'intersection-observer';
+
 import Vue from 'vue';
 import App from './App.vue';
 
--- src/widgetObserver.js
+++ src/widgetObserver.js
@@ -1,3 +1,9 @@
+/*
+  Документация по Intersection Observer API https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+  Для использования на публичных сайтах рекомендуется использовать полифилл https://github.com/w3c/IntersectionObserver/tree/master/polyfill
+  В данном проекте полифилл подключен в файле main.js
+*/
+
 const LOG_TITLE = '[Widget Observer]:';
 
 export default class WidgetObserver {
@@ -15,14 +21,32 @@ export default class WidgetObserver {
     console.log(`${LOG_TITLE} call init`);
 
     if (!this.widgeListElement || typeof this.widgeListElement === 'undefined') {
-      console.error('Not found DOM element');
+      console.error('Not found DOM element for widget list');
       return;
     }
+
+    /*
+      Intersection Observer в intersectionRatio возвращает процент
+      попадания элемента во vieport относительно высоты элемента.
+      Если высота элемента больше высоты viewport,
+      то есть вероятность, что коллбек просто не сработает.
+      Для такого случая нужно скорректировать threshold.
+    */
+    const { height } = this.widgeListElement.getBoundingClientRect();
+    if (!height) {
+      console.error('Failed to get height for widget list element');
+      return;
+    }
+
+    const thresholdCoef = window.innerHeight > height
+      ? 1
+      : window.innerHeight / height;
 
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.5,
+      // 50% процентов от высоты widgeListElement с дополнительной коррекцией
+      threshold: 0.5 * thresholdCoef,
     };
 
     const callback = (entries) => {
@@ -40,12 +64,22 @@ export default class WidgetObserver {
   }
 
   sendEvents() {
+    // В данном случае можно сразу сформировать массив из события `w_show` и трех событий `i_show`
     const events = [
       { type: 'w_show' },
       ...this.items.map(item => ({ type: 'i_show', itemId: item.item_id })),
     ];
+    /*
+      И отправить их за один раз.
+
+      aiturec('event', 'w_events', {
+        widgetId: this.widget,
+        events,
+      });
+    */
     console.log(`${LOG_TITLE} sendEvents`, events);
 
+    // После отправки массива событий необходимо остановить наблюдение за списком рекомендаций
     this.destroy();
   }
 
